a3a454e8428f2afd233bb7e1c426edfb3765a344
201912025 <michi2012@naver.com>
2025-04-11 14:29:25 +0900

    feat: 초기설정 및 elk 설정 구현

---
+++ src/test/java/com/example/advertise_service/AdvertiseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advertise_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertiseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
