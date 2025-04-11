7369dd8a8678508cbcc6581e96515d6a9957985e
201912025 <michi2012@naver.com>
2025-04-11 14:44:39 +0900

    feat: 광고 엔티티 구현

---
+++ src/main/java/com/example/advertise_service/entity/Advertisement.java
@@ -0,0 +1,41 @@
+package com.example.advertise_service.entity;
+
+import com.example.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "advertisements")
+public class Advertisement extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advertisementId;
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    @Builder
+
+    public Advertisement(String title, String content, String imageUrl, LocalDateTime startDate, LocalDateTime endDate) {
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
